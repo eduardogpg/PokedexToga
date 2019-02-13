4b983cad2bd33b92a93b09e65d4824072d07f7c1
Eduardo Ismael García Pérez <eduardo78d@gmail.com>
2019-02-13 15:44:54 -0600

    Next and previous

---
+++ app/consts.py
@@ -0,0 +1,13 @@
+import os
+
+WIDHT = 600
+HEIGHT = 500
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ICON_DIR = os.path.join(BASE_DIR, 'icons')
+
+METAPOD_ICON = os.path.join(ICON_DIR, 'metapod.png')
+BULBASAUR_ICON = os.path.join(ICON_DIR, 'bulbasaur.png')
+
+
+POKE_API = "https://pokeapi.co/api/v2/pokemon-form"
--- app/main.py
+++ app/main.py
@@ -1,100 +1,86 @@
-import os
 import toga
 import requests
 
-# TODO: Size for Main window, No re size, Get 20 first elements, draw pokemon image
-# TODO: Set new Icon!
-
-from toga.style.pack import *
-
-base_dir = os.path.dirname(os.path.abspath(__file__))
-icon_dir = os.path.join(base_dir, 'icons')
+from consts import *
+from toga.style.pack import Pack, CENTER
 
 class Pokedex(toga.App):
     def __init__(self, name, app_id):
         toga.App.__init__(self, name, app_id)
 
-        self.size = (600, 500)
-
-        self.pokemons = []
-        self.headings = ['Pokemon name']
+        self.name = name
+        self.size = (WIDHT, HEIGHT)
 
         self.offset = 0
         self.limit = 20
+        self.loading = False
 
-        self.table = self.create_table()
-        self.next_command, self.previous_command = self.create_tools()
-        self.content = self.create_content()
+        self.headings = ['Name']
+        self.pokemon_list = []
 
         self.load_pokemon()
 
     def startup(self):
         self.main_window = toga.MainWindow('main', title=self.name, size=self.size)
-        self.main_window.toolbar.add(self.next_command, self.previous_command)
+
+        self.table = self.create_table()
+        self.information_area = self.create_information_area()
 
         split = toga.SplitContainer()
-        split.content = [self.table, self.content]
+        split.content = [self.table, self.information_area]
+
+        next_command = self.create_next_command()
+        previous_command = self.create_previous_command()
 
         self.main_window.content = split
+        self.main_window.toolbar.add(next_command, previous_command)
+
         self.main_window.show()
+
+    def create_table(self):
+        table = toga.Table(self.headings, on_select=self.select_element)
+
+        return table
+
+    def create_information_area(self):
+        information = toga.Box()
+
+        return information
 
     def select_element(self, widget, row):
         print("Elemento seleccionado!")
 
-    def create_table(self):
-        table = toga.Table(headings=self.headings,
-                            on_select=self.select_element)
-
-        return table
-
-    def create_content(self):
-        content = toga.Box(style=Pack(direction=COLUMN, padding_top=10))
-
-        description = toga.ScrollContainer(horizontal=False)
-        description.content = content
-
-        return description
-
-    def create_tools(self):
-        next = toga.Command(self.next, label='Siguiente',
-            icon=os.path.join(icon_dir, 'bulbasaur.png')
-        )
-        
-        previous = toga.Command(self.previous, label='Anterior',
-            icon=os.path.join(icon_dir, 'metapod.png')
-        )
-
-        return next, previous
-
     def next(self, widget):
         self.offset += 1
-        self.load_pokemon()
 
     def previous(self, widget):
         self.offset -= 1
-        self.load_pokemon()
 
-    def validate_next_command(self):
-        self.previous_command.enabled = not self.offset == 0
+    def validate_commands(self):
+        pass
 
-    def get_pokemon_url(self):
-        return 'https://pokeapi.co/api/v2/pokemon-form?offset={}&limit={}'.format(self.offset, self.limit)
+    def create_next_command(self):
+        return toga.Command(self.next, label='Siguiente', icon=BULBASAUR_ICON)
+
+    def create_previous_command(self):
+        return toga.Command(self.previous, label='Anterior', icon=METAPOD_ICON)
 
     def load_pokemon(self):
-        self.pokemons.clear()
+        self.pokemon_list.clear()
 
-        response = requests.get(self.get_pokemon_url())
-        if response:
-            results = response.json()
-            pokemons = results.get('results', [])
+        path = "{}?offset={}&limit={}".format(POKE_API, self.offset, self.limit)
 
-            for pokemon in pokemons:
-                name = pokemon.get('name', '')
-                self.pokemons.append(name)
+        response = requests.get(path)
+        if response.status_code == 200:
+            result = response.json()
 
-        self.table.data = self.pokemons
-        self.validate_next_command()
+            for pokemon in result.get('results', []):
+                name = pokemon.get('name')
+                self.pokemon_list.append(name)
+
+    def get_pokemon(self, id):
+        path = "{}/{}".format(POKE_API, id)
 
 if __name__ == '__main__':
-    app = Pokedex('Podedex', 'com.codigofacilito.Podedex')
+    app = Pokedex('Pokedex', 'com.codigofacilito.Podedex')
     app.main_loop()
